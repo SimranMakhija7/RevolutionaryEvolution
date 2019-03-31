bbf6329ea9ec99b087385013a06eab78166d3f7a
sonali0123 <sonalipatel23600@gmail.com>
2019-03-31 19:16:32 +0530

    app.js modified

--- app.js
+++ app.js
@@ -85,6 +85,12 @@ app.get("/show/:name/:time",function(req,res){
 });
 
 //server
-app.listen(3000, function(){
-console.log("The Server Has Started on localhost 3000!");
-});
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
+
+
+
+
+// app.listen(3000, function(){
+// console.log("The Server Has Started on localhost 3000!");
+// });
